a796960a878030a730288da31654cbffc35390cf
velopert <public.velopert@gmail.com>
2019-11-19 03:12:14 +0900

    Use typescript

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,4 @@
 import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.(js|mdx)$/), module);
+configure(require.context('../src', true, /\.stories\.(js|mdx|tsx)$/), module);
--- src/Hello/Hello.tsx
+++ src/Hello/Hello.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
+type HelloProps = {
+  /** 보여주고 싶은 이름 */
+  name: string;
+  /** 이 값을 `true` 로 설정하면 h1 태그로 렌더링합니다. */
+  big?: boolean;
+  /** Hello 버튼 누를 때 호출 할 함수 */
+  onHello?: () => void;
+  /** Bye 버튼 누를 때 호출 할 함수 */
+  onBye?: () => void;
+};
 
 /**
  * 안녕하세요 라고 보여주고 싶을 땐 `Hello` 컴포넌트를 사용하세요.
@@ -7,7 +17,7 @@ import PropTypes from 'prop-types';
  * - `big` 값을 `true`로 설정하면 **크게** 나타납니다.
  * - `onHello` 와 `onBye` props로 설정하여 버튼이 클릭했을 때 호출 할 함수를 지정 할 수 있습니다.
  */
-const Hello = ({ name, big, onHello, onBye }) => {
+const Hello = ({ name, big, onHello, onBye }: HelloProps) => {
   return (
     <div>
       {big ? <h1>안녕하세요, {name}!</h1> : <p>안녕하세요, {name}!</p>}
@@ -17,17 +27,6 @@ const Hello = ({ name, big, onHello, onBye }) => {
       </div>
     </div>
   );
-};
-
-Hello.propTypes = {
-  /** 보여주고 싶은 이름 */
-  name: PropTypes.string.isRequired,
-  /** 이 값을 `true` 로 설정하면 h1 태그로 렌더링 됩니다. */
-  big: PropTypes.bool,
-  /** Hello 버튼 누를 때 호출할 함수 */
-  onHello: PropTypes.func,
-  /** Bye 버튼 누를 때 호출할 함수 */
-  onBye: PropTypes.func
 };
 
 Hello.defaultProps = {
