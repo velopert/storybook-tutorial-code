0ae5595917e747af5fafea2b989a2acb80d56b0b
velopert <public.velopert@gmail.com>
2019-11-20 02:00:08 +0900

    Create Button component

---
+++ src/Button/Button.stories.tsx
@@ -0,0 +1,112 @@
+/** @jsx jsx */
+import Button from './Button';
+import { jsx, css } from '@emotion/core';
+import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+
+export default {
+  title: 'components|Button',
+  component: Button,
+  decorators: [withKnobs]
+};
+
+export const button = () => {
+  const label = text('children', 'BUTTON');
+  const size = select('size', ['small', 'medium', 'big'], 'medium');
+  const theme = select(
+    'theme',
+    ['primary', 'secondary', 'tertiary'],
+    'primary'
+  );
+  const disabled = boolean('disabled', false);
+  const width = text('width', '');
+
+  return (
+    <Button
+      size={size}
+      theme={theme}
+      disabled={disabled}
+      width={width}
+      onClick={action('onClick')}
+    >
+      {label}
+    </Button>
+  );
+};
+
+button.story = {
+  name: 'Default'
+};
+
+export const primaryButton = () => {
+  return <Button>PRIMARY</Button>;
+};
+
+export const secondaryButton = () => {
+  return <Button theme="secondary">SECONDARY</Button>;
+};
+
+export const tertiaryButton = () => {
+  return <Button theme="tertiary">TERTIARY</Button>;
+};
+
+const buttonWrapper = css`
+  .description {
+    margin-bottom: 0.5rem;
+  }
+  & > div + div {
+    margin-top: 2rem;
+  }
+`;
+
+export const sizes = () => {
+  return (
+    <div css={buttonWrapper}>
+      <div>
+        <div className="description">Small</div>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div>
+        <div className="description">Medium</div>
+        <Button size="medium">BUTTON</Button>
+      </div>
+      <div>
+        <div className="description">Big</div>
+        <Button size="big">BUTTON</Button>
+      </div>
+    </div>
+  );
+};
+
+export const disabled = () => {
+  return (
+    <div css={buttonWrapper}>
+      <div>
+        <Button disabled>PRIMARY</Button>
+      </div>
+      <div>
+        <Button disabled theme="secondary">
+          SECONDARY
+        </Button>
+      </div>
+      <div>
+        <Button disabled theme="tertiary">
+          TERTIARY
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export const customSized = () => {
+  return (
+    <div css={buttonWrapper}>
+      <div>
+        <Button width="20rem">CUSTOM WIDTH</Button>
+      </div>
+      <div>
+        <Button width="100%">FULL WIDTH</Button>
+      </div>
+    </div>
+  );
+};
---
+++ src/Button/Button.tsx
@@ -0,0 +1,122 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+
+type ButtonProps = {
+  /** 버튼 안의 내용 */
+  children: React.ReactNode;
+  /** 클릭했을 때 호출할 함수 */
+  onClick?: (e?: React.MouseEvent<HTMLButtonElement>) => void;
+  /** 버튼의 생김새를 설정합니다. */
+  theme: 'primary' | 'secondary' | 'tertiary';
+  /** 버튼의 크기를 설정합니다 */
+  size: 'small' | 'medium' | 'big';
+  /** 버튼을 비활성화 시킵니다. */
+  disabled?: boolean;
+  /** 버튼의 너비를 임의로 설정합니다. */
+  width?: string | number;
+};
+
+/** `Button` 컴포넌트는 어떠한 작업을 트리거 할 때 사용합니다.  */
+const Button = ({
+  children,
+  theme,
+  size,
+  disabled,
+  width,
+  onClick
+}: ButtonProps) => {
+  return (
+    <button
+      css={[style, themes[theme], sizes[size], { width }]}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  theme: 'primary',
+  size: 'medium'
+};
+
+const style = css`
+  outline: none;
+  border: none;
+  box-sizing: border-box;
+  border-radius: 0.25rem;
+  line-height: 1;
+  font-weight: 600;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  &:focus {
+    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2);
+  }
+  &:disabled {
+    cursor: not-allowed;
+  }
+`;
+
+const themes = {
+  primary: css`
+    background: #20c997;
+    color: white;
+    &:hover:enabled {
+      background: #38d9a9;
+    }
+    &:active:enabled {
+      background: #12b886;
+    }
+    &:disabled {
+      background: #aed9cc;
+    }
+  `,
+  secondary: css`
+    background: #e9ecef;
+    color: #343a40;
+    &:hover:enabled {
+      background: #f1f3f5;
+    }
+    &:active:enabled {
+      background: #dee2e6;
+    }
+    &:disabled {
+      color: #c6d3e1;
+    }
+  `,
+  tertiary: css`
+    background: none;
+    color: #20c997;
+    &:hover:enabled {
+      background: #e6fcf5;
+    }
+    &:active:enabled {
+      background: #c3fae8;
+    }
+    &:disabled {
+      color: #bcd9d0;
+    }
+  `
+};
+
+const sizes = {
+  small: css`
+    height: 1.75rem;
+    font-size: 0.75rem;
+    padding: 0 0.875rem;
+  `,
+  medium: css`
+    height: 2rem;
+    font-size: 0.875rem;
+    padding: 0 1rem;
+  `,
+  big: css`
+    height: 3rem;
+    font-size: 1.125rem;
+    padding: 0 1.5rem;
+  `
+};
+
+export default Button;
